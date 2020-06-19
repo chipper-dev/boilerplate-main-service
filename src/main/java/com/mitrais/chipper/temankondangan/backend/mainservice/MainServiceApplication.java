ea392270bfa15d2f6918053b9befd839a7b19785
akbarb24 <akbar.binaji@gmail.com>
2020-06-19 09:49:13 +0800

    init

---
+++ src/main/java/com/mitrais/chipper/temankondangan/backend/mainservice/MainServiceApplication.java
@@ -0,0 +1,23 @@
+package com.mitrais.chipper.temankondangan.backend.mainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableHystrix
+@SpringBootApplication
+@EnableAutoConfiguration
+public class MainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServiceApplication.class, args);
+	}
+
+}
